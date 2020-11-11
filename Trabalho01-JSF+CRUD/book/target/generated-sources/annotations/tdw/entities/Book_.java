2c2f07f08f9ef7a2e84560904c389b76419ca0ce
Rafael Batista <rafael.batista@live.com>
2020-11-11 17:04:45 -0300

    atualizações no pom, xhtml e arquivos de compilação

--- Trabalho01-JSF+CRUD/book/target/generated-sources/annotations/tdw/entities/Book_.java
+++ Trabalho01-JSF+CRUD/book/target/generated-sources/annotations/tdw/entities/Book_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-02T11:54:36")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-11T16:16:55")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
